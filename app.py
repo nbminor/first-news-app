34038e50ec380376e4cf2617728f76cf97a941da
admin <admin@admins-Mac-Pro-294.local>
2016-03-12 09:58:10 -0700

    detail page for each victim

--- app.py
+++ app.py
@@ -15,7 +15,14 @@ def index():
     object_list = get_csv("./static/la-riots-deaths.csv")
     return render_template(template, object_list=object_list)
 
+@app.route('/<row_id>/')
+def detail(row_id):
+    template = "detail.html"
+    object_list = get_csv("./static/la-riots-deaths.csv")
+    for row in object_list:
+        if row['id'] == row_id:
+            return render_template(template, object=row)
+
 if __name__ == '__main__':
     #Fire up the Flask test server
     app.run(debug=True, use_reloader=True)
-
